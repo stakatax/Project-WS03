3b5719b57dfb04e05ffb65813d47965fa2f70fbf
stakatax <janfortun14@gmail.com>
2026-05-19 13:40:17 +0800

    validation of registration & imrpoving code structure

--- WS03/App/views/listings/edit.view.php
+++ WS03/App/views/listings/edit.view.php
@@ -11,13 +11,9 @@
             <h2 class="text-2xl font-bold mb-6 text-center text-gray-500">
                 Job Info
             </h2>
-            <?php if (isset($errors)) : ?>
-                <?php foreach ($errors as $error) : ?>
-                    <div class="message bg-red-100 p-3 my-3">
-                        <?= $error; ?>
-                    </div>
-                <?php endforeach; ?>
-            <?php endif; ?>
+            <?= loadPartials('errors', [
+                'errors' => $errors ?? []
+            ]) ?>
             <div class="mb-4">
                 <input
                     type="text"
--- WS03/helpers.php
+++ WS03/helpers.php
@@ -35,11 +35,12 @@ function loadView($name, $data = [])
  * @return void
  */
 
-function loadPartials($name)
+function loadPartials($name, $data = [])
 {
     $partialPath = basePath("App/views/partials/{$name}.php");
 
     if (file_exists($partialPath)) {
+        extract($data);
         require $partialPath;
     } else {
         echo "Partial {$name} not found";
